83f4a0048f3b66e225194cce9f163b867523bb9f
BMSales <bruno.m.sales03@gmail.com>
2025-09-18 12:05:48 -0300

    finished hello window

--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 #include "glad/include/glad/glad.h"
 #include <GLFW/glfw3.h>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+  glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window){
+  if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
+    glfwSetWindowShouldClose(window, true);
+  }
+}
+
 int main(){
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -17,12 +27,22 @@ int main(){
       return -1;
   }
   glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
     std::cout << "Failed to initialize GLAD" << std::endl;
     return -1;
   } 
 
-  glViewport(0, 0, 800, 600);
+  while(!glfwWindowShouldClose(window)){
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    processInput(window);
+    glfwSwapBuffers(window);
+    glfwPollEvents();    
+  }
+
+  glfwTerminate();
   return 0;
 }
